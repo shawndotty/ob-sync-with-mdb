1892e12e382f6d6e965226e2287c97eaa8adfcd7
johnnylearns <shawndotty@gmail.com>
2025-12-01 16:13:52 +0800

    change version up control

--- src/ui/settings-tab.ts
+++ src/ui/settings-tab.ts
@@ -40,152 +40,22 @@ export class OBSyncWithMDBSettingTab extends PluginSettingTab {
 			remoteValidator: () => this.apiService.checkApiKey(),
 		});
 
-		new Setting(containerEl)
-			.setName(t("Sync Scripts Update API Key"))
-			.setDesc(t("Please enter a valid update API Key"))
-			.addText((text) => {
-				const validSpan = createEl("span", {
-					text: t("Valid API Key"),
-					cls: "valid-text",
-				});
-				const loadingSpan = createEl("span", {
-					text: t("Validating..."),
-					cls: "loading-text",
-				});
-				validSpan.style.display = "none";
-				loadingSpan.style.display = "none";
-				text.inputEl.parentElement?.insertBefore(
-					validSpan,
-					text.inputEl
-				);
-				text.inputEl.parentElement?.insertBefore(
-					loadingSpan,
-					text.inputEl
-				);
-
-				const updateValidState = (
-					isValid: boolean,
-					isLoading: boolean = false
-				) => {
-					if (isLoading) {
-						text.inputEl.removeClass("valid-api-key");
-						text.inputEl.removeClass("invalid-api-key");
-						validSpan.style.display = "none";
-						loadingSpan.style.display = "inline";
-					} else {
-						loadingSpan.style.display = "none";
-						if (isValid) {
-							text.inputEl.removeClass("invalid-api-key");
-							text.inputEl.addClass("valid-api-key");
-							text.inputEl.style.borderColor = "#4CAF50";
-							text.inputEl.style.color = "#4CAF50";
-							validSpan.style.display = "inline";
-						} else {
-							text.inputEl.removeClass("valid-api-key");
-							text.inputEl.addClass("invalid-api-key");
-							text.inputEl.style.borderColor = "#FF5252";
-							text.inputEl.style.color = "#FF5252";
-							validSpan.style.display = "none";
-						}
-					}
-				};
-
-				// 初始状态设置
-				updateValidState(this.plugin.settings.updateAPIKeyIsValid);
-				return text
-					.setPlaceholder(t("Enter the API Key"))
-					.setValue(this.plugin.settings.updateAPIKey)
-					.onChange(async (value) => {
-						this.plugin.settings.updateAPIKey = value;
-						if (isValidApiKey(value)) {
-							updateValidState(false, true); // 显示加载状态
-							await new ApiService(
-								this.plugin.settings
-							).checkApiKey();
-							updateValidState(
-								this.plugin.settings.updateAPIKeyIsValid
-							);
-						} else {
-							updateValidState(false);
-						}
-						await this.plugin.saveSettings();
-					});
-			});
-
-		new Setting(containerEl)
-			.setName(t("Your Email Address"))
-			.setDesc(
-				t(
-					"Please enter the email you provided when you purchase this product"
-				)
-			)
-			.addText((text) => {
-				const validSpan = createEl("span", {
-					text: t("Valid Email"),
-					cls: "valid-text",
-				});
-				const loadingSpan = createEl("span", {
-					text: t("Validating..."),
-					cls: "loading-text",
-				});
-				validSpan.style.display = "none";
-				loadingSpan.style.display = "none";
-				text.inputEl.parentElement?.insertBefore(
-					validSpan,
-					text.inputEl
-				);
-				text.inputEl.parentElement?.insertBefore(
-					loadingSpan,
-					text.inputEl
-				);
-
-				const updateValidState = (
-					isValid: boolean,
-					isLoading: boolean = false
-				) => {
-					if (isLoading) {
-						text.inputEl.removeClass("valid-email");
-						text.inputEl.removeClass("invalid-email");
-						validSpan.style.display = "none";
-						loadingSpan.style.display = "inline";
-					} else {
-						loadingSpan.style.display = "none";
-						if (isValid) {
-							text.inputEl.removeClass("invalid-email");
-							text.inputEl.addClass("valid-email");
-							text.inputEl.style.borderColor = "#4CAF50";
-							text.inputEl.style.color = "#4CAF50";
-							validSpan.style.display = "inline";
-						} else {
-							text.inputEl.removeClass("valid-email");
-							text.inputEl.addClass("invalid-email");
-							text.inputEl.style.borderColor = "#FF5252";
-							text.inputEl.style.color = "#FF5252";
-							validSpan.style.display = "none";
-						}
-					}
-				};
-
-				// 初始状态设置
-				updateValidState(this.plugin.settings.userChecked);
-				return text
-					.setPlaceholder(t("Enter your email"))
-					.setValue(this.plugin.settings.userEmail)
-					.onChange(async (value) => {
-						this.plugin.settings.userEmail = value;
-						if (isValidEmail(value)) {
-							updateValidState(false, true);
-							console.log("here");
-							await new ApiService(
-								this.plugin.settings
-							).getUpdateIDs();
-							updateValidState(this.plugin.settings.userChecked);
-						} else {
-							updateValidState(false);
-						}
-						await this.plugin.saveSettings();
-					});
-			});
+		this.createValidatedInput({
+			containerEl,
+			name: t("Your Email Address"),
+			description: t(
+				"Please enter the email you provided when you purchase this product"
+			),
+			placeholder: t("Enter your email"),
+			reload: true,
+			getValue: () => this.plugin.settings.userEmail,
+			setValue: (value) => (this.plugin.settings.userEmail = value),
+			getIsValid: () => this.plugin.settings.userChecked,
+			setIsValid: (isValid) =>
+				(this.plugin.settings.userChecked = isValid),
+			localValidator: isValidEmail,
+			remoteValidator: () => this.apiService.getUpdateIDs(),
+		});
 
 		new Setting(containerEl)
 			.setName(t("Sync Scripts Folder"))
--- version-bump.mjs
+++ version-bump.mjs
@@ -9,6 +9,12 @@ manifest.version = targetVersion;
 writeFileSync("manifest.json", JSON.stringify(manifest, null, "\t"));
 
 // update versions.json with target version and minAppVersion from manifest.json
-let versions = JSON.parse(readFileSync("versions.json", "utf8") || '{}');
+let versions;
+try {
+  versions = JSON.parse(readFileSync("versions.json", "utf8"));
+} catch (error) {
+  console.error("Error parsing versions.json, initializing with empty object:", error);
+  versions = {};
+}
 versions[targetVersion] = minAppVersion;
 writeFileSync("versions.json", JSON.stringify(versions, null, "\t"));
